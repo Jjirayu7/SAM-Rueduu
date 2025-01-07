174b3fd13f55c3b517366bc76daa5b36104aa11b
Jjirayu7 <jirayuwat.nawa@kmutt.ac.th>
2025-01-08 01:33:49 +0700

    main-update-cart.js

--- homepage/app/src/components/Button.js
+++
@@ -1,7 +0,0 @@
-import react from "react";
-import PropTypes from "prop-types";
-
-const shapes = {
-    round: "rounded-[14px]",
-}
-
--- homepage/app/src/pages/Cart.js
+++ homepage/app/src/pages/Cart.js
@@ -1,27 +1,9 @@
-import { Helmet } from 'react-helmet';
-import { Button, Heading, Text, Img} from '../components';
+
 
 function Cart() {
   return (
     <>
-      <Helmet>
-        <title>Cart</title>
-        <meta name="description" content="Cart page" />
-      </Helmet>
-      <div className='flex w-full items-center bg-50_01 md:flex-col'>
-        <div className='w-[14%] self-end md:w-full md:px-5'>
-          <div className='flex flex-col items-center gap-[958px] lg:gap-[718px] md:gap-[718px] sm:gap-[479px]'>
-            <div className='flex items-center gap-2'>
-              <img src="images/img_menu.svg" alt="Menu" className="h-[52px] w-[50%] object-contain" />
-              <Heading as='h1' 
-              className="self-end text-[32pxl] font-bold !text-purple-900_01 lg:text-[27px] md:text-[26px] sm:text-[24px]">
-                Menu</Heading>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
+      
     </>
   );
 }
